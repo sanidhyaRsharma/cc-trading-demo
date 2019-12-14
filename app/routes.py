01adf9c68250fc8b8f0e53176517626678730c60
kaustubhgaikwad <kaustubhgaikwad1998@gmail.com>
2019-12-14 18:48:07 +0530

    added pending-requests and send-request page

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template
+from flask import request
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
@@ -8,18 +10,20 @@ def index():
 
 @app.route('/buy')
 def buy():
-<<<<<<< HEAD
     sellers=[{"Name":"abc","CarbonCredits":10},{"Name":"def","CarbonCredits":20},{"Name":"xyz","CarbonCredits":50}]
     return render_template('buy.html',len=len(sellers),sellers=sellers)
 
-@app.route('/send-request')
-def send_request(data):
-    print(seller)
-    return render_template('send-request.html',seller=data)
-=======
-    return render_template('buy.html')
+@app.route('/send-request',methods=['GET', 'POST'])
+def send_request():
+    seller_data=request.form.to_dict()
+    return render_template('send-request.html',data=seller_data)
+    
 
 @app.route('/sell')
 def sell():
     return render_template('sell.html')
->>>>>>> 8375a9b06387511086f4dbb1ada6c5773cc3c9d7
+
+@app.route('/requests')
+def requests():
+    requests=[{"Name":"abc","CarbonCredits":10},{"Name":"def","CarbonCredits":20},{"Name":"xyz","CarbonCredits":50}]
+    return render_template('requests.html',len=len(requests),requests=requests)
